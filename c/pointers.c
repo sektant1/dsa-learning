a7d1da621f6f7941ec893502f5e6f832968cdd9d
sektant-gabe <fergabe@pm.me>
2025-12-11 10:32:41 -0300

    add more info to pointers in C

--- c/pointers.c
+++ c/pointers.c
@@ -1,5 +1,10 @@
-// Pense nos '*' como uma CHAVE no formato de estrela que destrava o acesso a um
-// endereco de memoria, como uma CHAVE que abre um BAU.
+// Ponteiro eh uma variavel que armazena o endereco na memoria de alguma outra
+// variavel. Utilidade: eles ajudam a evitar o desperdicio de memoria,
+// permitindo que voce passe apenas o endereco de uma estrutura de dados GRANDE
+// ao inves de copiar ela com todos os seus dados.
+
+// Pense nos ponteiros(*) como uma CHAVE no formato de estrela que destrava o
+// acesso a um endereco de memoria, como uma CHAVE que abre um BAU.
 
 #include <stdio.h>
 
