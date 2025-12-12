b73c9b5582591c3316cb2191ae02def64a90cf53
sektant-gabe <fergabe@pm.me>
2025-12-12 15:02:47 -0300

    fix some format

--- c/job.c
+++ c/job.c
@@ -6,13 +6,15 @@
 
 typedef struct job
 {
-    struct job    *next;  // next active job
-    char          *command;  // command line used for messages
+    struct job    *next;           // next active job
+    char          *command;        // command line used for messages
     process       *first_process;  // list of processes in this job
-    pid_t          pgid;  // process group ID
-    char           notified;  // true if user told about stopped job
-    struct termios tmodes;  // saved terminal modes
-    int            stdin, stdout, stderr;  // standard i/o channels
+    pid_t          pgid;           // process group ID
+    char           notified;       // true if user told about stopped job
+    struct termios tmodes;         // saved terminal modes
+    int            stdin;          // standard input channel
+    int            stdout;         // standard output channel
+    int            stderr;         // standard error channel
 } job;
 
 job *first_job = NULL;
--- c/process.h
+++ c/process.h
@@ -4,10 +4,10 @@
 // A process is a single process, duh
 typedef struct process
 {
-    struct process *next;  // next process in the pipeline
-    char          **argv;  // for exec
-    pid_t           pid;  // process ID
+    struct process *next;       // next process in the pipeline
+    char          **argv;       // for exec
+    pid_t           pid;        // process ID
     char            completed;  // true if process has completed
-    char            stopped;  // true if process has stopped
-    int             status;  // reported status value
+    char            stopped;    // true if process has stopped
+    int             status;     // reported status value
 } process;
